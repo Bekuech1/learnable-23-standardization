c183371f7940ebf6940d353a7960491fbfc632e0
bekuech1 <igboanugobekuechi22@yahoo.com>
2024-06-03 22:48:15 +0100

    second commit

--- BANDAGE/src/components/navbar/Navbar.jsx
+++ BANDAGE/src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import './Navbar.css'
 
 export default function Navbar() {
     return(
+        <>
         <div className="navbar-body">
             <div className="navbar-left">
                 <h3>Bandage</h3>
@@ -27,6 +28,23 @@ export default function Navbar() {
                 </div>
             </div>
         </div>
+        <div className='navbar2'>
+            <div className='nav1'>
+                <h3>Bandage</h3>
+                <div className='nav2'>
+                    <button id='search'><img src="icons/search2.svg" alt="" /></button>
+                    <button><a href="/shoppingcart"><img src="icons/cart2.svg" alt="" /></a></button>
+                    <button><img src="icons/menu.svg" alt="" /></button>
+                </div>
+            </div>
+            <div className='nav3'>
+                <a href="/">Home</a>
+                <a href="">product</a>
+                <a href="">pricing</a>
+                <a href="">contact</a>
+            </div>
+        </div>
+        </>
 
     )
 }
